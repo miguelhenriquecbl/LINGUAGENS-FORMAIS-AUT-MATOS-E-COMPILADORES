db40f9b09424897456e674af3b7bc44a6a49f7c9
lukiwo0 <miguelhenrique2701@gmail.com>
2026-03-30 18:58:46 -0300

    add lexical analyzer results

---
+++ Trabalho/analisador-lexico/exemplos-saida/ts/saidaTS-teste003.ts
@@ -0,0 +1,16 @@
+LEXEMA               TIPO
+------               ----
+program              KW_PROGRAM
+var                  KW_VAR
+integer              KW_INTEGER
+real                 KW_REAL
+begin                KW_BEGIN
+end                  KW_END
+if                   KW_IF
+then                 KW_THEN
+else                 KW_ELSE
+while                KW_WHILE
+do                   KW_DO
+comentarios          ID
+x                    ID
+y                    ID
---
+++ Trabalho/analisador-lexico/exemplos-saida/ts/saidaTS-teste005.ts
@@ -0,0 +1,15 @@
+LEXEMA               TIPO
+------               ----
+program              KW_PROGRAM
+var                  KW_VAR
+integer              KW_INTEGER
+real                 KW_REAL
+begin                KW_BEGIN
+end                  KW_END
+if                   KW_IF
+then                 KW_THEN
+else                 KW_ELSE
+while                KW_WHILE
+do                   KW_DO
+erroarroba           ID
+x                    ID
