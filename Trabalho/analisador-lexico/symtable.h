c21a804692fddf23c0ede484a05d2ad1a549179a
lukiwo0 <miguelhenrique2701@gmail.com>
2026-03-30 18:56:34 -0300

    add EntradaTS struct and scope functions

---
+++ Trabalho/analisador-lexico/symtable.h
@@ -0,0 +1,22 @@
+#ifndef SYMTABLE_H
+#define SYMTABLE_H
+#include <stdio.h>
+
+typedef struct {
+    char lexema[256];
+    char tokenName[32];
+} EntradaTS;
+
+void inicializaTS();
+
+int buscaTS(char *lexema);
+
+void inserirTS(char *lexema, char *tokenName);
+
+void escreverTS(FILE *ts);
+
+void liberaTS();
+
+char *pegarTokenNome (int idx);
+
+#endif
